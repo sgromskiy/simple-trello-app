7b1b13e4ae0f917fc00968f1a1100e155f87e47f
UNIVERSEsgromskiy <Sergey.Gromskiy@dataart.com>
2020-01-29 14:41:59 +0200

    added build

---
+++ precache-manifest.d4959334d88c1e8c0583e0a2056031a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a18bbf82ee931a3d0fc593662cf9b4aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d17690652478e4ba030",
+    "url": "/static/css/main.9df720b5.chunk.css"
+  },
+  {
+    "revision": "991e3d6d1b5a3ec59f4c",
+    "url": "/static/js/2.870c7ad6.chunk.js"
+  },
+  {
+    "revision": "1d17690652478e4ba030",
+    "url": "/static/js/main.fb1b795a.chunk.js"
+  },
+  {
+    "revision": "2c79ffa9237396cb6231",
+    "url": "/static/js/runtime-main.5d51f42f.js"
+  },
+  {
+    "revision": "3f7ae562428ea8dae8bb43231a4784a9",
+    "url": "/static/media/revan.3f7ae562.png"
+  }
+]);
